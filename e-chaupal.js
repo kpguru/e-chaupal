2ba17a0284169137fce580118cecf6e3e9cd5895
Kapil <kapil@grepruby.com>
2014-12-23 12:44:08 +0530

    add news feed by admin

--- e-chaupal.js
+++ e-chaupal.js
@@ -1,3 +1,5 @@
+NewsFeedUrls = new Mongo.Collection("news_feed_urls");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
@@ -30,6 +32,14 @@ if (Meteor.isClient) {
   Template.home.helpers({
     toi_feeds: function () {
       return Session.get("toi_feeds");
+    },
+    
+    is_admin: function(current_user_name){
+      return current_user_name == "admin-echaupal"
+    },
+    
+    feed_urls: function(){
+      return NewsFeedUrls.find({}, {sort: {createdAt: -1}});
     }
   });
   
@@ -45,6 +55,31 @@ if (Meteor.isClient) {
       Meteor.call("getNewsByTopics", url, function(error, results) {
         Session.set("toi_by_topics", results.data.NewsItem);
       });
+    },
+    
+    'submit form' : function (event) {
+      var news_name = event.target.news_name.value;
+      var news_category = event.target.news_category.value;
+      var feed_url = event.target.feed_url.value;      
+      
+      NewsFeedUrls.insert({
+        news_name: news_name,
+        news_category: news_category,
+        feed_url: feed_url,
+        createdAt: new Date()
+      });
+
+      // Clear form
+      event.target.news_name.value = "";
+      event.target.news_category.value = "";
+      event.target.feed_url.value = "";
+      
+      // Prevent default form submit
+      return false;
+    },
+    
+    "click .delete": function () {
+      NewsFeedUrls.remove(this._id);
     }
   });
   
