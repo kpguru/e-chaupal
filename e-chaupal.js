25240881d956bdc94e1aceccc251d7e3ac7a61e0
Kapil <kapil@grepruby.com>
2014-12-25 15:40:14 +0530

    change in flow

--- e-chaupal.js
+++ e-chaupal.js
@@ -7,6 +7,7 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
   Session.setDefault("toi_by_topics", "");
+  Session.setDefault("news_categories", "");
 
   Router.map(function(){
     this.route('home', {path: '/'});
@@ -28,8 +29,17 @@ if (Meteor.isClient) {
       return current_user_name == "admin-echaupal"
     },
     
-    toi_feeds: function () {
+    feed_urls: function(){
       return NewsFeedUrls.find({}, {sort: {createdAt: -1}});
+    },
+    
+    get_category_name: function(id){
+      return Categories.findOne({_id: id}).name;
+    },
+    
+    get_news_display_name: function(id){
+      var news_id = Categories.findOne({_id: id}).news_id;
+      return News.findOne({_id: news_id}).name;
     }
   });
   
@@ -51,19 +61,16 @@ if (Meteor.isClient) {
   //~ Feed block start
   Template.feed.events({    
     'submit form' : function (event) {
-      var news_id = event.target.news_id.value;
       var category_id = event.target.category_id.value;
       var feed_url = event.target.feed_url.value;      
       
       NewsFeedUrls.insert({
-        news_id: news_id,
         category_id: category_id,
         feed_url: feed_url,
         createdAt: new Date()
       });
 
       // Clear form
-      event.target.news_id.value = "";
       event.target.category_id.value = "";
       event.target.feed_url.value = "";
       
@@ -73,7 +80,14 @@ if (Meteor.isClient) {
     
     "click .delete": function () {
       NewsFeedUrls.remove(this._id);
-    }
+    },
+
+     "change select": function(event,template){
+        var news_id = event.target.value;
+        categories = Categories.find({news_id: news_id});
+        console.log(categories);
+        Session.set(news_categories, categories);
+     }
   });
   
   Template.feed.helpers({
@@ -83,8 +97,11 @@ if (Meteor.isClient) {
     news: function(){
       return News.find({}, {sort: {createdAt: -1}});
     },
-    news_categories: function(){
+    categories: function(){
       return Categories.find({}, {sort: {createdAt: -1}});
+    },
+    news_categories: function(){
+      return Session.get(news_categories);
     },
     
     get_news_name: function(id){
@@ -93,6 +110,11 @@ if (Meteor.isClient) {
     
     get_category_name: function(id){
       return Categories.findOne({_id: id}).name;
+    },
+    
+    get_news_display_name: function(id){
+      var news_id = Categories.findOne({_id: id}).news_id;
+      return News.findOne({_id: news_id}).name;
     }
   });
   
